5d02a55d36ba35d2b6219802e24480c50e763297
DebuggedMoon <predojevic@proton.me>
2024-07-15 13:12:52 +0200

    Replace usage of mock data with server action query

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,34 +1,36 @@
-import { Question, QUESTIONS } from "@/utils/Questions";
+import getMultibleChoiseQuestion from "@/services/getMultibleChoiseQuestion";
 import shuffleArrayInPlace from "@/utils/shuffleArray";
 
-// Mock-data TODO: replace with real question logic
-const firstQuestion: Question = QUESTIONS[0];
+export default async function Pratice() {
 
-const renderedAnswers = [
-	...firstQuestion.answers, 
-	firstQuestion.correctAnswer
-]
+	const question = await getMultibleChoiseQuestion();
 
-shuffleArrayInPlace(renderedAnswers);
-//
+	if (question === null) {
+		return (
+			<main className="flex flex-col items-center justify-center min-h-screen p-24">
+				No questions found!
+			</main>
+		)
+	}
 
-export default function Pratice() {
+	shuffleArrayInPlace(question.answers);
+
 	return (
 		<main className="flex flex-col min-h-screen p-24">
 
-			<h1 className="py-[10%] text-3xl text-center">{firstQuestion.question}</h1>
-
-			
+			<h1 className="py-[10%] text-3xl text-center">{question.body}</h1>
 
 			<ul className="flex flex-col gap-6 h-auto">
 				{
-					renderedAnswers.map(
-						answer => (
-							<li key={answer} className="flex justify-center">
-								<button className="border-2 border-gray-300 hover:border-gray-500 rounded w-[75%] pl-6 py-2 text-left">{answer}</button>
-							</li>
-						)						
-					)
+					question.answers
+						.filter(answer => answer.body != "")
+						.map(
+							answer => (
+								<li key={answer.body} className="flex justify-center">
+									<button className="border-2 border-gray-300 hover:border-gray-500 rounded w-[75%] pl-6 py-2 text-left">{answer.body}</button>
+								</li>
+							)						
+						)
 				}
 			</ul>
 
