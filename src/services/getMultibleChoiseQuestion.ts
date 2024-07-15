11b3159d747badfe0127365eb7385d82d3989109
DebuggedMoon <predojevic@proton.me>
2024-07-15 13:05:40 +0200

    Add randomization to getMultibleChoiseQuestion

--- src/services/getMultibleChoiseQuestion.ts
+++ src/services/getMultibleChoiseQuestion.ts
@@ -3,8 +3,13 @@
 import prisma from "./prismaService";
 
 export default async function getMultibleChoiseQuestion() {
+	
+	const questionCount = await prisma.multibleChoiseQuestion.count();
 
 	const question = await prisma.multibleChoiseQuestion.findFirst({
+		where: {
+			id: Math.ceil(Math.random() * questionCount)
+		},
 		include: {
 			answers: true
 		}
