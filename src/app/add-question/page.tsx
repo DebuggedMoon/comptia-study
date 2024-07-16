447dc9bb501090b263be96c662e90097c3b768a2
DebuggedMoon <predojevic@proton.me>
2024-07-16 12:22:23 +0200

    Improve styling of /add-question page

--- src/app/add-question/page.tsx
+++ src/app/add-question/page.tsx
@@ -7,13 +7,51 @@ export default function AddQuestion() {
 
 			<h1 className="my-5 text-4xl text-center font-semibold">Add practice question</h1>
 			
-			<form action={createMultibleChoiseQuestion} className="flex flex-col max-w-[800px] gap-5">
-				<input name="question" type="text" className="border" placeholder="Question here" />
-				<input name="correctAnswer" type="text" className="border" placeholder="Correct Answer here" />
-				<input name="answer" type="text" className="border" placeholder="Answer here" />
-				<input name="answer" type="text" className="border" placeholder="Answer here" />
-				<input name="answer" type="text" className="border" placeholder="Answer here" />
-				<button type="submit" className="border">Create Question</button>
+			<form 
+				action={createMultibleChoiseQuestion} 
+				className="flex flex-col min-w-[300px] max-w-[500px] gap-5 my-8"
+			>
+
+				<input 
+					name="question" 
+					type="text" 
+					className="border border-zinc-400 roundex px-3 h-10" 
+					placeholder="Question here" 
+				/>
+
+				<input 
+					name="correctAnswer" 
+					type="text" 
+					className="border border-zinc-300 roundex px-3 h-10" 
+					placeholder="Correct Answer here" 
+				/>
+				
+				<input 
+					name="answer" 
+					type="text" 
+					className="border border-zinc-300 roundex px-3 h-10" 
+					placeholder="Answer here" 
+				/>
+
+				<input 
+					name="answer" 
+					type="text" 
+					className="border border-zinc-300 roundex px-3 h-10" 
+					placeholder="Answer here" 
+				/>
+
+				<input 
+					name="answer" 
+					type="text" 
+					className="border border-zinc-300 roundex px-3 h-10" 
+					placeholder="Answer here" 
+				/>
+
+				<button 
+					type="submit" 
+					className="h-10 bg-black px-5 rounded text-white my-5"
+				>Create Question</button>
+
 			</form>
 
 		</main>
