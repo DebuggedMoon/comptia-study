541bac6785e651e0d2cd0a6c0a37dfc9d6504dd9
DebuggedMoon <predojevic@proton.me>
2024-07-21 00:04:38 +0200

    Add dynamic amount of answers to question creation

--- src/app/add-question/page.tsx
+++ src/app/add-question/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import createMultibleChoiseQuestion from "@/services/createMultibleChoiseQuestion";
+import { useState } from "react";
 
 export default function AddQuestion() {
+	const [answerAmount, setAnswerAmount] = useState(1);
 
 	return (
 		<main className="flex flex-col items-center min-h-screen p-24">
@@ -26,33 +30,32 @@ export default function AddQuestion() {
 					placeholder="Correct Answer here" 
 				/>
 				
-				<input 
-					name="answer" 
-					type="text" 
-					className="border border-zinc-300 roundex px-3 h-10" 
-					placeholder="Answer here" 
-				/>
-
-				<input 
-					name="answer" 
-					type="text" 
-					className="border border-zinc-300 roundex px-3 h-10" 
-					placeholder="Answer here" 
-				/>
-
-				<input 
-					name="answer" 
-					type="text" 
-					className="border border-zinc-300 roundex px-3 h-10" 
-					placeholder="Answer here" 
-				/>
+				{
+					new Array(answerAmount).fill("").map(
+						(_, i) => (
+							<input
+								key={i}
+								name="answer" 
+								type="text" 
+								className="border border-zinc-300 roundex px-3 h-10" 
+								placeholder="Answer here" 
+							/>
+						)
+					)
+				}
 
 				<button 
 					type="submit" 
-					className="h-10 bg-black px-5 rounded text-white my-5"
+					className="h-10 bg-black px-5 rounded text-white mt-5"
 				>Create Question</button>
 
 			</form>
+
+			
+			<button 
+				onClick={() => setAnswerAmount(answerAmount + 1)}
+				className="h-10 border border-black px-5 rounded"
+			>Add Answer</button>
 
 		</main>
 	);
