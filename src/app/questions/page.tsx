8e6d1cf06129ce305fd1602c52aeb15229d52267
DebuggedMoon <predojevic@proton.me>
2024-07-26 23:37:57 +0200

    Add basic content for /questions page

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,8 +1,22 @@
+import getListOfQuestions from "@/services/getListOfQuestions"
 
-export default function Questions() {
+export default async function Questions() {
+
+	const questions = await getListOfQuestions(); 
+
 	return (
-		<>
+		<main className="flex flex-col items-center min-h-screen p-24">
+			<ul>
+				{
+					questions.map(question => (
+						<li 
+							key={question.id}
+							className="border-2 rounded my-3 p-3"
+						>{question.body}</li>
+					))
+				}
+			</ul>
 			{/* TODO: list questions and allow for them to be modified / deleted */}
-		</>
+		</main>
 	)
 }
