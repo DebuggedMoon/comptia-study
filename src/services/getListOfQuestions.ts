9f4264f218e19e2496e677cc69fea27dc7649023
DebuggedMoon <predojevic@proton.me>
2024-07-30 16:09:55 +0200

    Fix getListOfQuestions action not returning answers

--- src/services/getListOfQuestions.ts
+++ src/services/getListOfQuestions.ts
@@ -4,7 +4,11 @@ import prisma from "./prismaService";
 
 export default async function getListOfQuestions() {
 	
-	const questions = await prisma.multibleChoiseQuestion.findMany()
+	const questions = await prisma.multibleChoiseQuestion.findMany({
+		include: {
+			answers: true
+		}
+	})
 
 	return questions
 }
