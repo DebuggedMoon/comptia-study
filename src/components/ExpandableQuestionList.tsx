72b067c28ec38f8d4382d0d35c991b94651e09ba
DebuggedMoon <predojevic@proton.me>
2024-07-30 16:18:36 +0200

    Add client component for displaying expandable list of questions

---
+++ src/components/ExpandableQuestionList.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { MultibleChoiseAnswer, MultibleChoiseQuestion } from "@prisma/client";
+import { useState } from "react";
+
+type ComponentParameters = {
+	questions: {
+		id: number,
+		body: string,
+		hasSingleCorrectAnswer: boolean,
+		answers: MultibleChoiseAnswer[]
+	}[]
+}
+
+export default function ExpandableQuestionList({ questions }: ComponentParameters) {
+
+	const [expanededQuestionId, setExpandedQuestionId] = useState(-1)
+
+	return (
+		<ul>
+		{
+			questions.map(question => (
+				<li 
+					key={question.id}
+					className="border-2 rounded my-3 p-3"
+				>
+					<p>{question.body}</p>
+					<button
+						className="border p-2"
+						onClick={() => {
+
+							if (question.id === expanededQuestionId) {
+								setExpandedQuestionId(-1);
+								return;
+							}
+
+							setExpandedQuestionId(question.id)
+						}}
+					>{expanededQuestionId === question.id ? "Collapse" : "Expanded"}</button>
+				{
+					expanededQuestionId === question.id ? (
+						<ul>
+							{
+								question.answers.map(answer => (
+									<li key={answer.id}>{answer.body}</li>
+								))
+							}
+						</ul>
+					) : ""
+				}
+				</li>
+			))
+		}
+	</ul>
+	)
+
+}
