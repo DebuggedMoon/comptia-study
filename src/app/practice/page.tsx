19f634e0f857e92599d046ed83ef5618861b2450
DebuggedMoon <predojevic@proton.me>
2024-08-02 22:58:15 +0200

    Replace old practice page with page for choosing practice modes

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,25 +1,34 @@
-import MultibleChoiseAnswerButton from "@/components/MultibleChoiseAnswerButton";
-import getMultibleChoiseQuestion from "@/services/getMultibleChoiseQuestion";
-import shuffleArrayInPlace from "@/utils/shuffleArray";
+import Link from "next/link";
+import { title } from "process";
 
-export default async function Pratice() {
-
-	const question = await getMultibleChoiseQuestion();
-
-	if (question === null) {
-		return (
-			<main className="flex flex-col items-center justify-center min-h-screen p-24">
-				No questions found!
-			</main>
-		)
+const PRACTICE_MODES = [
+	{
+		title: "Multible Choise",
+		href: "multiblechoise"
 	}
+];
 
-	shuffleArrayInPlace(question.answers);
-
+export default function Practice() {
 	return (
 		<main className="flex flex-col min-h-screen p-24">
-			<h1 className="py-[10%] text-3xl text-center">{question.body}</h1>
-			<MultibleChoiseAnswerButton answers={question.answers} />
+			<h1 className="text-center text-4xl font-bold">Practice</h1>
+			<ul className="flex gap-5 m-10">
+				{
+					PRACTICE_MODES.map(
+						mode => (
+							<li 
+								key={mode.href}
+								className="w-[150px] h-[150px] border shadow rounded"
+							>
+								<Link
+								className="flex justify-center items-center h-full w-full font-bold"
+									href={`./practice/${mode.href}`}
+								>{mode.title}</Link>
+							</li>
+						)
+					)
+				}
+			</ul>
 		</main>
-	);
+	);	
 }
