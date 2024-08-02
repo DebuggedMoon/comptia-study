7f7f070075ab7a4c178dc6fe2cb38f27d8436316
DebuggedMoon <predojevic@proton.me>
2024-08-02 22:55:02 +0200

    Move multible choise practice mode into its own page

---
+++ src/app/practice/multiblechoise/page.tsx
@@ -0,0 +1,25 @@
+import MultibleChoiseAnswerButton from "@/components/MultibleChoiseAnswerButton";
+import getMultibleChoiseQuestion from "@/services/getMultibleChoiseQuestion";
+import shuffleArrayInPlace from "@/utils/shuffleArray";
+
+export default async function PracticeMultibleChoise() {
+
+	const question = await getMultibleChoiseQuestion();
+
+	if (question === null) {
+		return (
+			<main className="flex flex-col items-center justify-center min-h-screen p-24">
+				No questions found!
+			</main>
+		)
+	}
+
+	shuffleArrayInPlace(question.answers);
+
+	return (
+		<main className="flex flex-col min-h-screen p-24">
+			<h1 className="py-[10%] text-3xl text-center">{question.body}</h1>
+			<MultibleChoiseAnswerButton answers={question.answers} />
+		</main>
+	);
+}
