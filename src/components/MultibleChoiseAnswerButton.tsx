9b5a3d918a20914e8ce2ed66e35d946134f60668
DebuggedMoon <predojevic@proton.me>
2024-08-15 00:29:38 +0200

    Fix component param types and add callback for handling answer submittion

--- src/components/MultibleChoiseAnswerButton.tsx
+++ src/components/MultibleChoiseAnswerButton.tsx
@@ -1,25 +1,19 @@
 "use client";
 
+import { MultibleChoiseAnswer, MultibleChoiseQuestion } from "@prisma/client";
 import { useState } from "react";
 
 type ComponentParams = {
-	answers: {
-		id: number,
-		isCorrect: boolean,
-		body: string,
-		questionId: number
-	}[]
+	answers: MultibleChoiseAnswer[],
+	onAnswer: () => any
 }
 
-export default function MultibleChoiseAnswerButton({ answers }: ComponentParams) {
+export default function MultibleChoiseAnswerButton({ answers, onAnswer }: ComponentParams) {
 	const [revealAnswers, setRevealAnswers] = useState(false);
 
 	function onAnswerClick() {
 		setRevealAnswers(true);
-		setTimeout(
-			() => window.location.reload(), 
-			1500
-		)
+		setTimeout(onAnswer || (() => window.location.reload()), 1500)
 	}
 
 	return (
@@ -28,7 +22,7 @@ export default function MultibleChoiseAnswerButton({ answers }: ComponentParams)
 				answers
 					.filter(answer => answer.body != "")
 					.map(
-						answer => (
+						(answer) => (
 							<li key={answer.id} className="flex justify-center">
 								<button onClick={onAnswerClick} className={`border-2 ${revealAnswers ? (answer.isCorrect ? "border-green-500" : "border-red-500") : "border-gray-300 hover:border-gray-500"} rounded w-[75%] pl-6 py-2 text-left shadow-xl`}>{answer.body}</button>
 							</li>
