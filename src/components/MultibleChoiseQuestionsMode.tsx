710ee9e9fc8e60e442336035934b087bc6b6be6f
DebuggedMoon <predojevic@proton.me>
2024-08-19 11:54:04 +0200

    Add info on amount of correct answers to end of multible choise mode

--- src/components/MultibleChoiseQuestionsMode.tsx
+++ src/components/MultibleChoiseQuestionsMode.tsx
@@ -3,7 +3,7 @@
 import MultibleChoiseAnswerButton from "@/components/MultibleChoiseAnswerButton";
 import ModeProgressBar from "@/components/ModeProgressBar";
 import { MultibleChoiseAnswer, MultibleChoiseQuestion } from "@prisma/client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type ComponentParameters = {
 	questions: (MultibleChoiseQuestion & {
@@ -15,15 +15,27 @@ type ComponentParameters = {
 export default function PracticeMultibleChoise({ questions, numberOfQuestions }: ComponentParameters) {
 
 	const [questionNumber, setQuestionNumber] = useState(0);
+	const correctAnswers = useRef(0);
 
 	if (questionNumber === numberOfQuestions) {
 		return (
 			<main className="flex flex-col min-h-screen max-h-screen p-24">
 				<ModeProgressBar progress={100} />
 				<h1 className="pb-24 pt-10 text-3xl text-center">Finished!</h1>
-				<div className="flex justify-center mt-24">
+				<div className="flex flex-col items-center my-8">
+					<div
+						className="flex gap-28 [&>h3]:text-2xl [&>h3]:font-bold [&>h3]:border-2 [&>h3]:shadow-xl [&>h3]:rounded [&>h3]:p-3"
+					>
+						<h3
+							className="text-green-600"
+						>Correct Answers: {correctAnswers.current}</h3>
+						
+						<h3
+							className="text-red-600"
+						>Wrong Answers: {numberOfQuestions - correctAnswers.current}</h3>
+					</div>
 					<button
-						className="w-40 h-12 text-white bg-black rounded"
+						className="w-40 h-12 text-white bg-black rounded mt-48"
 						onClick={() => {
 							window.location.reload()	
 						}}
@@ -39,7 +51,13 @@ export default function PracticeMultibleChoise({ questions, numberOfQuestions }:
 			<h1 className="pb-24 pt-10 text-3xl text-center">{questions[questionNumber].body}</h1>
 			<MultibleChoiseAnswerButton
 				answers={questions[questionNumber].answers}
-				onAnswer={() => setQuestionNumber(questionNumber + 1)}
+				onAnswer={(isCorrect) => {
+					if (isCorrect) {
+						correctAnswers.current++;
+					}
+
+					setQuestionNumber(questionNumber + 1);
+				}}
 			/>
 		</main>
 	);
